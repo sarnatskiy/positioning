1a3f22d8c96340931c1da2bf098998d7f537ef72
Dimas <zdm2507@gmail.com>
2026-04-07 17:59:09 +0500

    4

--- index.js
+++ index.js
@@ -15,3 +15,24 @@ overlay.addEventListener('click', (e) => {
         overlay.classList.remove('active');
     }
 });
+const progressBar = document.querySelector('.progress');
+
+function fillProgressBar(duration) {
+    let startTime = null;
+
+    function animate(timestamp) {
+        if (!startTime) {
+            startTime = timestamp;
+        }
+        const progress = timestamp - startTime;
+        const percentage = Math.min(progress / duration * 100, 100);
+        progressBar.style.width = percentage + '%';
+
+        if (progress < duration) {
+            requestAnimationFrame(animate);
+        }
+    }
+    requestAnimationFrame(animate);
+}
+
+fillProgressBar(3000);
